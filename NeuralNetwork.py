19ca464d7b5153174c82cc8be79b3d77a4633abe
BenAltermatt <ben.altermatt20@gmail.com>
2021-11-22 14:44:37 -0500

    Finished basic NN arhcitecture
    Next I want to add some functionality to the class

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,40 +1,29 @@
 import random
-import Node
+from Node import Node
 
 class NeuralNetwork: # a neural network made of nodes
     # for now, it will be a traditional, fully connected network
-    __num_inputs = 0
-    __num_outputs = 0
-    __normalize = False
-    __layers = None
-    __output_layer = None
 
     # finds the number of input and output values and
     # whether the inputs should be normalized  between 0 and 1
     def __init__(self, num_inputs=1, num_outputs=1, normalize=False):
-        global __num_inputs
-        global __num_outputs
-        global __normalize
-        global __layers 
-        global __output_layer
-
-        __num_inputs = num_inputs
-        __num_outputs = num_outputs
-        __normalize = True
-        __layers = list()
-        __output_layer = self.add_output()
+        self.__num_inputs = num_inputs
+        self.__num_outputs = num_outputs
+        self.__normalize = normalize
+        self.__layers = list()
+        self.__output_layer = self.add_output()
 
     # create an output_layer
     def add_output(self, activation_function=None):
         final_layer = list()
         # we need to know the number of inputs
         # for the input node
-        num_inputs = __num_inputs
+        num_inputs = self.__num_inputs
 
-        if len(__layers) > 0: # we have a middle layer
-            num_inputs = len(__layers[-1])
+        if len(self.__layers) > 0: # we have a middle layer
+            num_inputs = len(self.__layers[-1])
         
-        for x in range(__num_outputs):
+        for x in range(self.__num_outputs):
             if activation_function is not None:
                 final_layer.append(Node(num_inputs, active_func=activation_function))
             else:
@@ -50,13 +39,13 @@ class NeuralNetwork: # a neural network made of nodes
 
         # make it set to the largest
         if index == -1:
-            index = len(__layers)
+            index = len(self.__layers)
 
         # find out if this is the first layer
-        if len(__layers) == 0:
-            num_weights = __num_inputs
+        if len(self.__layers) == 0:
+            num_weights = self.__num_inputs
         else:
-            num_weights = len(__layers[-1])
+            num_weights = len(self.__layers[-1])
 
         # build each node and add it to the layer
         for x in range(n) :
@@ -65,4 +54,23 @@ class NeuralNetwork: # a neural network made of nodes
             else:
                 node_group.append(Node(num_weights))
 
+        self.__layers.insert(index, node_group)
+
         self.add_output()
+
+    # we need a string representation of this for
+    # better testing
+    def __str__(self):
+        retStr = ""
+        for x in range(len(self.__layers)):
+            retStr += "\nLayer " + str(x) + "\n========\n"
+            print(retStr)
+            for y in self.__layers[x]:
+                retStr += str(y) + "\n"
+            retStr += "========\n"
+        return retStr
+
+testNet = NeuralNetwork(2, 1)
+testNet.add_layer(3)
+testNet.add_layer(4)
+print(testNet)
