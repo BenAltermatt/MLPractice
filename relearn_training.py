274d3046fd5e44e55dca7433610cd7445b1c0400
BenAltermatt <ben.altermatt20@gmail.com>
2021-11-25 20:04:50 -0500

    Got the output working
    I want to get the back propagation set up next but I really need to sit down and focus to figure it out.

--- relearn_training.py
+++ relearn_training.py
@@ -1,8 +1,6 @@
 import numpy as np
 import random, math
 
-from Old_AI.HillClimb.HillClimb import sigmoid
-
 """
 I have no clue what the hell I did in high school this shit
 is crazy. I am going to relearn it. 
@@ -24,10 +22,10 @@ def gen_brain(structure, w_min, w_max, b_min, b_max, k_min, k_max):
         weights = list()
         biases = list()
         sigmoid_constants = list()
-        for x in range(structure[x]):
+        for y in range(structure[x]):
             # generate the weight vector for this perceptron
             w_vec = list()
-            for y in range(structure[x - 1]):
+            for z in range(structure[x - 1]):
                 w_vec.append(random.random() * (w_max - w_min) + w_min)
             weights.append(tuple(w_vec))
 
@@ -35,25 +33,47 @@ def gen_brain(structure, w_min, w_max, b_min, b_max, k_min, k_max):
             biases.append(random.random() * (b_max - b_min) + b_min)
 
             #generate a sigmoid constant list for this perceptron
-            sigmoid_constants.append(random.random() * (k_max - k_min) + k_max)
+            sigmoid_constants.append(random.random() * (k_max - k_min) + k_min)
 
         # make the weights matrix
         w_matrix = np.matrix(tuple(weights))
 
         # make the bias matrix
-        b_matrix = np.matrix(tuple(biases,))
+        b_matrix = np.matrix(tuple(biases)).transpose()
 
         # combine all info into a tuple representing a layer
-        layers.append(tuple(w_matrix, b_matrix, tuple(sigmoid_constants)))
-    
+        layers.append((w_matrix, b_matrix, tuple(sigmoid_constants)))
+        
     return tuple(layers)
 
+# this will forward propagate through the network and get the results
+def run_network(input_vals, network, activation):
+
+    output = np.matrix(input_vals).transpose()
+
+    # goes through each layer
+    for x in network:
+        ys = x[0]* output + x[1] # Y= MX + B
+        # now we need to perform the activation function to get the outputs
+        activs = list()
+        ys = ys.tolist()
+        for y in range(len(ys)):
+            activs.append(activation(ys[y][0], x[2][y])) # pass the k value for the layer
+        output = np.matrix(activs).transpose()
+    
+    # evetnually we shopuld get through all of our
+    # layers and have the final output
+    return output.tolist()[0]
+
+# sigmoid function used as a basic activation
+def sigmoid(x, k):
+    return 1 / (1 + math.exp(-k * x))
+
+# this will print out the brain
 def print_brain(brain):
     print(brain)
 
 
 test_brain = gen_brain([2, 4, 1], -1, 1, -1, 1, 0, 5)
-print_brain(test_brain)
+print(run_network((0, 0), test_brain, sigmoid))
         
-        
-
